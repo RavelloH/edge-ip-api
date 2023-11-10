8ce3042c03a3dd4d33363482a443718f9d0acc6a
RavelloH <hyh20060327@qq.com>
2023-11-10 23:04:21 +0800

    Opti. 优化输出

--- api/ip.js
+++ api/ip.js
@@ -6,17 +6,16 @@ export const config = {
 };
 
 export default function handler(request) {
-  return new Response(
-    {
-      ip: ipAddress(request) || "unknown",
-      city: geolocation(request).city,
-      country: geolocation(request).country,
-      latitude: geolocation(request).latitude,
-      longitude: geolocation(request).longitude,
-      region: geolocation(request).region,
-    },
-    {
-      headers: { "content-type": "text/json" },
-    },
-  );
+  const data = {
+    ip: ipAddress(request) || "unknown",
+    city: geolocation(request).city,
+    country: geolocation(request).country,
+    latitude: geolocation(request).latitude,
+    longitude: geolocation(request).longitude,
+    region: geolocation(request).region,
+  };
+
+  return new Response(JSON.stringify(data), {
+    headers: { "content-type": "application/json" },
+  });
 }
